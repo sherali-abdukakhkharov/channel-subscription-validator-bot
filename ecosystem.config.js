b9095d9840ae7d7bdb87756a3e7e7ae5d2b19ef6
Sherali Abdukakhkharov <sherali@Sheralis-Mini.localdomain>
2026-01-12 01:04:20 +0500

    feat/add-pm2-config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'channel-subscription-validator-bot',
+      cwd: '/root/projects/channel-subscription-validator-bot',
+      script: './dist/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      log_file: './logs/combined.log',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      restart_delay: 4000,
+    },
+  ],
+}
